fc032f11ce1d6dc0002a921fd98edd0a30e8fd7e
Lena Kononchuk <kononchuk.elena91@gmail.com>
2021-11-30 12:29:13 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import News from './components/News/News';
 import MainContent from './components/MainContent/MainContent';
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Switch} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
@@ -12,16 +12,18 @@ class App extends React.Component {
     return (
       // BrowserRouter’s responsibility to store all the components and its routes as an object.
       //Router is a statement that holds the specific path of the app along with the component’s name and renders it once it matches the URL.
-      <BrowserRouter>
+      <HashRouter>
         <main className="for_page">
           <Header />
             <div className="pages">
+            <Switch>
               <Route path="/" exact component={MainContent} /> 
               <Route path="/news" exact component={News} />
+             </Switch>
             </div>
         <Footer />
         </main>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
